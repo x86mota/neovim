cc8f6e9389a5525e973ed98bedbb04f456bbf23c
x86mota <fmc.sh@protonmail.com>
2025-10-08 22:37:21 -0300

    plugin: update lualine

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -31,8 +31,8 @@ return {
 			options = {
 				icons_enabled = true,
 				theme =	'auto',
-				component_separators = { left = '', right = '' },
-				section_separators = { left = '', right = '' },
+				component_separators = { left = '', right = '' },
+				section_separators = { left = '', right = '' },
 				disabled_filetypes = {
 					statusline = {'neo-tree'},
 					tabline = {'neo-tree'}
